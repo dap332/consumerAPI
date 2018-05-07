da2dcd739984f740a9e2973a3533d38f61fff5fa
Dhruv <dap332@drexel.edu>
2018-05-07 13:55:29 +0200

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+const server = require('express')();
+const bodyparser = require('body-parser');
+
+const PORT = 80;
+
+server.use(bodyparser.json());
+server.use(bodyparser.urlencoded({ extended: false}));
+
+server.get('/', (req, res) =>{
+    res.send(`<h1> Hello </h1>`);
+});
+
+server.post('/', (req, res) => {
+    try {
+        let data = JSON.parse(JSON.stringify(req.body));
+
+        res.end('ok');
+    }catch (e) {
+        res.send("error" + req.body);
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
